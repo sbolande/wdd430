d9ff69fc6cabcf9cc01b5e1815c18fe2c61e4b23
Seth Bolander <bol15015@byui.edu>
2022-09-18 19:57:43 -0600

    property binding

--- w02_p1_TheBasics/the-basics/src/app/servers/servers.component.ts
+++ w02_p1_TheBasics/the-basics/src/app/servers/servers.component.ts
@@ -4,14 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer == true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
 }
