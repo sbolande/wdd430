c9dd777f59c2decb5254de5c8657ec83a9be8a45
Seth Bolander <bol15015@byui.edu>
2022-09-18 19:22:43 -0600

    finished making components with CLI

--- w02_p1_TheBasics/the-basics/src/app/app.module.ts
+++ w02_p1_TheBasics/the-basics/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
