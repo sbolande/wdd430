fde9a4704a0ee7555ecbab0ed08deadcd70c39e7
Seth Bolander <bol15015@byui.edu>
2022-09-18 19:44:53 -0600

    created warning-alert component

--- w02_p1_TheBasics/the-basics/src/app/app.module.ts
+++ w02_p1_TheBasics/the-basics/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ w02_p1_TheBasics/the-basics/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css'],
+})
+export class WarningAlertComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
