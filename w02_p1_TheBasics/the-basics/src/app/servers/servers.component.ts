612ecbef7d760a8fc39a923942a4477fa991142e
Seth Bolander <bol15015@byui.edu>
2022-09-18 19:24:38 -0600

    working with component templates

--- w02_p1_TheBasics/the-basics/src/app/servers/servers.component.ts
+++ w02_p1_TheBasics/the-basics/src/app/servers/servers.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
