a33d2cbd80961e8ae333bae89030c7429befed0a
Seth Bolander <bol15015@byui.edu>
2022-09-18 19:17:15 -0600

    a few edits and adding server cmpt

--- w02_p1_TheBasics/the-basics/src/app/app.module.ts
+++ w02_p1_TheBasics/the-basics/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ServerComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
