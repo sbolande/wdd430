2ab6778a3aa48ab0e1c5000e5d574d43b5c94c42
Seth Bolander <bol15015@byui.edu>
2022-09-20 22:26:04 -0600

    two way databinding

--- w02_p1_TheBasics/the-basics/src/app/servers/servers.component.ts
+++ w02_p1_TheBasics/the-basics/src/app/servers/servers.component.ts
@@ -2,17 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
   templateUrl: './servers.component.html',
-  // template: `
-  //   <app-server></app-server>
-  //   <app-server></app-server>
-  // `,
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -21,4 +17,12 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
