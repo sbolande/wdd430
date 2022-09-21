7f295cef4352b6810971d0f6d990850ac9928f22
Seth Bolander <bol15015@byui.edu>
2022-09-21 17:19:06 -0600

    working with ngIf

--- w02_p1_TheBasics/the-basics/src/app/servers/servers.component.ts
+++ w02_p1_TheBasics/the-basics/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}.`;
   }
 
