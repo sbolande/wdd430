36edc7b897ff141a40e420c7a948d0a73fb0ab53
Seth Bolander <bol15015@byui.edu>
2022-09-22 23:01:09 -0600

    finishing touches on contact-list

--- w02_p2_Assignment/cms/src/app/contacts/contact.model.ts
+++ w02_p2_Assignment/cms/src/app/contacts/contact.model.ts
@@ -1,9 +1,9 @@
 export class Contact {
   constructor(
-    public id: number,
+    public id: string,
     public name: string,
     public email: string,
-    public phone: number,
+    public phone: string,
     public imageUrl: string,
     public group?: Contact[]
   ) {}
