890b7e8b7764f507e11ea3860afe0c2d97b6b665
Seth Bolander <bol15015@byui.edu>
2022-09-27 12:59:15 -0600

    view switch now works properly

--- w03_Assignment/cms/src/app/app.component.ts
+++ w03_Assignment/cms/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cms';
+  selectedFeature = 'contacts';
+
+  switchView(selectedFeature: string) {
+    if (this.selectedFeature.toUpperCase() !== selectedFeature.toUpperCase()) {
+      this.selectedFeature = selectedFeature;
+    }
+  }
 }
