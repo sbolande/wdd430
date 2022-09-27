a56bac98975860aa6fa59489e5ea74220159746a
Seth Bolander <bol15015@byui.edu>
2022-09-27 12:47:42 -0600

    added message components

--- w03_Assignment/cms/src/app/app.module.ts
+++ w03_Assignment/cms/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { DocumentsComponent } from './documents/documents.component';
 import { DocumentListComponent } from './documents/document-list/document-list.component';
 import { DocumentItemComponent } from './documents/document-item/document-item.component';
 import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +27,9 @@ import { DocumentDetailComponent } from './documents/document-detail/document-de
     DocumentListComponent,
     DocumentItemComponent,
     DocumentDetailComponent,
+    MessageItemComponent,
+    MessageEditComponent,
+    MessageListComponent,
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ w03_Assignment/cms/src/app/messages/message-item/message-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-item',
+  templateUrl: './message-item.component.html',
+  styleUrls: ['./message-item.component.css']
+})
+export class MessageItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
