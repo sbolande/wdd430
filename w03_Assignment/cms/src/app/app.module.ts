1ffb55227c268645b7fbe1fff3d84ad3a82ad77c
Seth Bolander <bol15015@byui.edu>
2022-09-27 11:58:10 -0600

    Created contact-item component

--- w03_Assignment/cms/src/app/app.module.ts
+++ w03_Assignment/cms/src/app/app.module.ts
@@ -6,9 +6,17 @@ import { HeaderComponent } from './header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ContactsComponent, ContactListComponent, ContactDetailComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactDetailComponent,
+    ContactItemComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
