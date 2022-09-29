a9bd8faef66484bb7e08f2b2cbf956715dc5932e
Seth Bolander <bol15015@byui.edu>
2022-09-29 11:42:36 -0600

    working with ngSwitch

--- Academind - Angular The Complete Guide/w04_Directives/directives-start/src/app/app.component.ts
+++ Academind - Angular The Complete Guide/w04_Directives/directives-start/src/app/app.component.ts
@@ -10,4 +10,6 @@ export class AppComponent {
   oddNumbers = this.numbers.filter((n) => n % 2 !== 0);
   evenNumbers = this.numbers.filter((n) => n % 2 === 0);
   onlyOdd = false;
+
+  value = 10;
 }
