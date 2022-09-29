2620917e126e8f683d7930c4a5d59775f1f37da9
Seth Bolander <bol15015@byui.edu>
2022-09-29 14:02:54 -0600

    implemented document-list

--- w04_Assignment/cms/src/app/documents/document-list/document-list.component.ts
+++ w04_Assignment/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Document } from '../document.model';
 
 @Component({
@@ -7,11 +7,21 @@ import { Document } from '../document.model';
   styleUrls: ['./document-list.component.css'],
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter();
+
   documents = [
-    new Document('1', 'Test', 'This is a test.', 'https://www.example.com/'),
+    new Document('1', 'Test 1', 'This is test 1.', 'https://www.example.com/1'),
+    new Document('2', 'Test 2', 'This is test 2.', 'https://www.example.com/2'),
+    new Document('3', 'Test 3', 'This is test 3.', 'https://www.example.com/3'),
+    new Document('4', 'Test 4', 'This is test 4.', 'https://www.example.com/4'),
+    new Document('5', 'Test 5', 'This is test 5.', 'https://www.example.com/5'),
   ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
