43045b83ec89eb0dc9d4ef893337e08ea411cb3a
Seth Bolander <bol15015@byui.edu>
2022-09-29 13:58:20 -0600

    implemented document item

--- w04_Assignment/cms/src/app/documents/document-item/document-item.component.ts
+++ w04_Assignment/cms/src/app/documents/document-item/document-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-item',
   templateUrl: './document-item.component.html',
-  styleUrls: ['./document-item.component.css']
+  styleUrls: ['./document-item.component.css'],
 })
 export class DocumentItemComponent implements OnInit {
+  @Input() document: Document;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
