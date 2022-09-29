2da5a8b7c0495d01590ab5f4d875327125eee7d4
Seth Bolander <bol15015@byui.edu>
2022-09-29 10:21:03 -0600

    messing around with ngClass ngStyle and directives

--- Academind - Angular The Complete Guide/w04_Directives/directives-start/src/app/app.component.ts
+++ Academind - Angular The Complete Guide/w04_Directives/directives-start/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
+  oddNumbers = this.numbers.filter((n) => n % 2 !== 0);
+  evenNumbers = this.numbers.filter((n) => n % 2 === 0);
   onlyOdd = false;
 }
--- Academind - Angular The Complete Guide/w04_Directives/directives-start/src/app/app.module.ts
+++ Academind - Angular The Complete Guide/w04_Directives/directives-start/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
+import { BetterHighlightDirective } from "./better-highlight/better-highlight.directive";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
