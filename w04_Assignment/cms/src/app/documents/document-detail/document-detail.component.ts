0467a9335a945d1f424ce1a91e3382d5ae1391b0
Seth Bolander <bol15015@byui.edu>
2022-09-29 13:53:48 -0600

    updates from Lesson 4 Files

--- w04_Assignment/cms/src/app/documents/document-detail/document-detail.component.ts
+++ w04_Assignment/cms/src/app/documents/document-detail/document-detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-detail',
   templateUrl: './document-detail.component.html',
-  styleUrls: ['./document-detail.component.css']
+  styleUrls: ['./document-detail.component.css'],
 })
 export class DocumentDetailComponent implements OnInit {
+  document: Document;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
