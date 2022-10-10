51066c68f0d002bef921a1f3dca071c2cb9421de
Seth Bolander <bol15015@byui.edu>
2022-10-10 12:04:40 -0600

    added AccountsService and LoggingService

--- Academind - Angular The Complete Guide/w05_Services/services-start/src/app/account/account.component.ts
+++ Academind - Angular The Complete Guide/w05_Services/services-start/src/app/account/account.component.ts
@@ -1,18 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { AccountsService } from "../accounts.service";
+import { LoggingService } from "../logging.service";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  selector: "app-account",
+  templateUrl: "./account.component.html",
+  styleUrls: ["./account.component.css"],
+  providers: [LoggingService, AccountsService],
 })
 export class AccountComponent {
-  @Input() account: {name: string, status: string};
+  @Input() account: { name: string; status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountsService.updateStatus(this.id, status);
+    this.loggingService.logStatusChange(status);
   }
 }
--- Academind - Angular The Complete Guide/w05_Services/services-start/src/app/new-account/new-account.component.ts
+++ Academind - Angular The Complete Guide/w05_Services/services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from "@angular/core";
+import { AccountsService } from "../accounts.service";
+import { LoggingService } from "../logging.service";
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  selector: "app-new-account",
+  templateUrl: "./new-account.component.html",
+  styleUrls: ["./new-account.component.css"],
+  providers: [LoggingService, AccountsService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
