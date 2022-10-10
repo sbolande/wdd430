75a558f7b81216a1289bbdbcb0a21aa2973ae73a
Seth Bolander <bol15015@byui.edu>
2022-10-10 15:03:52 -0600

    created contact service

---
+++ w05_Assignment/cms/src/app/contacts/contact.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Contact } from './contact.model';
+import { MOCKCONTACTS } from './MOCKCONTACTS';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactService {
+  private contacts: Contact[] = [];
+
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+  }
+
+  getContacts(): Contact[] {
+    return this.contacts.slice();
+  }
+
+  getContact(id: string): Contact {
+    return this.contacts.find((c) => c.id === id);
+  }
+}
