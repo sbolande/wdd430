0a07645f02b657a4f834197fcb4119744c2e6157
Seth Bolander <bol15015@byui.edu>
2022-10-13 16:09:28 -0600

    resolving dynamic data

--- Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/app.module.ts
+++ Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/app.module.ts
@@ -15,7 +15,8 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AuthService } from "./auth.service";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
-import { ErrorPageComponent } from './error-page/error-page.component';
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 @NgModule({
   declarations: [
@@ -30,7 +31,13 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     ErrorPageComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
+  providers: [
+    ServersService,
+    AuthService,
+    AuthGuard,
+    CanDeactivateGuard,
+    ServerResolver,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/servers/server/server.component.ts
+++ Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Router, ActivatedRoute, Params } from "@angular/router";
+import { Router, ActivatedRoute, Params, Data } from "@angular/router";
 
 import { ServersService } from "../servers.service";
 
@@ -18,11 +18,14 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const id = +this.route.snapshot.params["id"];
-    this.server = this.serversService.getServer(id);
-    this.route.params.subscribe((params: Params) => {
-      this.server = this.serversService.getServer(params["id"]);
+    this.route.data.subscribe((data: Data) => {
+      this.server = data["server"];
     });
+    // const id = +this.route.snapshot.params["id"];
+    // this.server = this.serversService.getServer(id);
+    // this.route.params.subscribe((params: Params) => {
+    //   this.server = this.serversService.getServer(params["id"]);
+    // });
   }
 
   onEdit() {
