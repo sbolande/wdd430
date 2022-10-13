d4e748d0b56f41901176ec7d753fdc3e19d75dac
Seth Bolander <bol15015@byui.edu>
2022-10-13 11:17:20 -0600

    route params and observables

--- Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/users/user/user.component.ts
+++ Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/users/user/user.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+  user: { id: number; name: string };
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params["id"],
+      name: this.route.snapshot.params["name"],
+    };
+    this.paramsSubscription.subscribe((params: Params) => {
+      this.user.id = params["id"];
+      this.user.name = params["name"];
+    });
   }
 
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
 }
