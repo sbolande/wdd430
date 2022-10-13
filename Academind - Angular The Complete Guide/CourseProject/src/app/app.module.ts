124b3ca89bc93255acccdf3611c37716247b0920
Seth Bolander <bol15015@byui.edu>
2022-10-13 17:37:35 -0600

    created AppRoutingModule

--- Academind - Angular The Complete Guide/CourseProject/src/app/app.module.ts
+++ Academind - Angular The Complete Guide/CourseProject/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -24,7 +25,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     ShoppingEditComponent,
     DropdownDirective,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })
