0a0ff8e6551b0d5a7064755ccf7d3d33d1c264b3
Seth Bolander <bol15015@byui.edu>
2022-10-13 16:01:03 -0600

    added static data

--- Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/app-routing.module.ts
+++ Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { ServerComponent } from "./servers/server/server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -32,7 +33,12 @@ const appRoutes: Routes = [
       },
     ],
   },
-  { path: "not-found", component: PageNotFoundComponent },
+  // { path: "not-found", component: PageNotFoundComponent },
+  {
+    path: "not-found",
+    component: ErrorPageComponent,
+    data: { message: "Page not found!" },
+  },
   { path: "**", redirectTo: "/not-found" },
 ];
 
--- Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/app.module.ts
+++ Academind - Angular The Complete Guide/w06_Routing/routing-start/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AuthService } from "./auth.service";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.s
     EditServerComponent,
     ServerComponent,
     PageNotFoundComponent,
+    ErrorPageComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
