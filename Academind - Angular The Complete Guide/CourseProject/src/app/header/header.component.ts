e8a426b4cd0190bb716b726e0e0435775a357f53
Seth Bolander <bol15015@byui.edu>
2022-10-13 17:45:46 -0600

    Fixed a few broken hrefs

--- Academind - Angular The Complete Guide/CourseProject/src/app/header/header.component.ts
+++ Academind - Angular The Complete Guide/CourseProject/src/app/header/header.component.ts
@@ -5,10 +5,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
-}
+export class HeaderComponent {}
