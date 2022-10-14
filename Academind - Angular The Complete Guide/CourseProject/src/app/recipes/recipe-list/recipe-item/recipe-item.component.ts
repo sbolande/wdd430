cef044a6616260827557ca0a6319c556411d1473
Seth Bolander <bol15015@byui.edu>
2022-10-13 18:11:02 -0600

    added and configured child routes

--- Academind - Angular The Complete Guide/CourseProject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Academind - Angular The Complete Guide/CourseProject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -10,11 +10,5 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {}
-
   ngOnInit(): void {}
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
