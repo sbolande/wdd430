7e377a6504ea48ae03fb997246a95cb1e49f7202
Seth Bolander <bol15015@byui.edu>
2022-10-13 19:16:39 -0600

    added top-level route links

--- w06_Assignment/cms/src/app/header.component.ts
+++ w06_Assignment/cms/src/app/header.component.ts
@@ -1,17 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-  @Output() selectedFeatureEvent = new EventEmitter<string>();
-
   constructor() {}
 
   ngOnInit(): void {}
-
-  onSelected(selectedEvent: string) {
-    this.selectedFeatureEvent.emit(selectedEvent);
-  }
 }
