4d0c25432d333a855dddbe650c568db1162caf9a
Seth Bolander <bol15015@byui.edu>
2022-10-14 14:14:02 -0600

    messing around with custom observables

--- Academind - Angular The Complete Guide/w07_Observables/obs-01-start/src/app/home/home.component.ts
+++ Academind - Angular The Complete Guide/w07_Observables/obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { interval, Observable, Subscription } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customIntervalObs = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3!"));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObs
+      .pipe(
+        filter((data) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return `Round: ${data + 1}`;
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (error) => {
+          console.error(error);
+          alert(error.message);
+        },
+        complete: () => {
+          console.log("COMPLETE!");
+        },
+      });
   }
 
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
