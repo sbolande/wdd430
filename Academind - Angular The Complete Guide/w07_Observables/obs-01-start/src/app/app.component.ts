df99bcd4cb69b87fe8cac399ddea773f085e8fe6
Seth Bolander <bol15015@byui.edu>
2022-10-14 15:01:23 -0600

    working with Subjects

--- Academind - Angular The Complete Guide/w07_Observables/obs-01-start/src/app/app.component.ts
+++ Academind - Angular The Complete Guide/w07_Observables/obs-01-start/src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { UserService } from "./user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.activatedEmitter.subscribe((didActivate) => {
+      this.userActivated = didActivate;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userService.activatedEmitter.unsubscribe();
+  }
 }
