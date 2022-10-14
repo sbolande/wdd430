0ecb8eb6ef7b4d8a5db849e9c8fa63b7d304ebbb
Seth Bolander <bol15015@byui.edu>
2022-10-13 19:11:05 -0600

    removed ngSwitch for router outlet

--- w06_Assignment/cms/src/app/app.component.ts
+++ w06_Assignment/cms/src/app/app.component.ts
@@ -7,11 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cms';
-  selectedFeature: string;
-
-  switchView(selectedFeature: string) {
-    if (this.selectedFeature?.toUpperCase() !== selectedFeature.toUpperCase()) {
-      this.selectedFeature = selectedFeature;
-    }
-  }
 }
