a5a7fce7a00f3570dbf61d348e9cecca1786db46
Seth Bolander <bol15015@byui.edu>
2022-10-13 19:20:22 -0600

    added empty DocumentEdit cmpt

--- w06_Assignment/cms/src/app/app.module.ts
+++ w06_Assignment/cms/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MessageEditComponent } from './messages/message-edit/message-edit.compo
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { AppRoutingModule } from './app-routing.module';
     MessageEditComponent,
     MessageListComponent,
     DropdownDirective,
+    DocumentEditComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
