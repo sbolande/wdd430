b373e80a6dfff024af5f4a52174cb0873fd748e9
Seth Bolander <bol15015@byui.edu>
2022-10-17 11:42:43 -0600

    working with two-way binding

--- Academind - Angular The Complete Guide/w08_FormHandling/forms-td-start/src/app/app.component.ts
+++ Academind - Angular The Complete Guide/w08_FormHandling/forms-td-start/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
+  defaultQuestion = "pet";
+  answer = "";
 
   suggestUserName() {
     const suggestedName = "Superuser";
