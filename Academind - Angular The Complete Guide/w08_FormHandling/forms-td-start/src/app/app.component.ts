cc11f21643578daecb49a88b713bfb6c684c15e5
Seth Bolander <bol15015@byui.edu>
2022-10-17 11:31:13 -0600

    adding validation and access to form

--- Academind - Angular The Complete Guide/w08_FormHandling/forms-td-start/src/app/app.component.ts
+++ Academind - Angular The Complete Guide/w08_FormHandling/forms-td-start/src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("f") signupForm: NgForm;
+
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
