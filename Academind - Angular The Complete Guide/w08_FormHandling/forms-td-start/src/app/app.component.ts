fc471cb966287a9cea030601af11090d01f08edb
Seth Bolander <bol15015@byui.edu>
2022-10-17 12:08:21 -0600

    broken link, updated from final zip

--- Academind - Angular The Complete Guide/w08_FormHandling/forms-td-start/src/app/app.component.ts
+++ Academind - Angular The Complete Guide/w08_FormHandling/forms-td-start/src/app/app.component.ts
@@ -7,10 +7,18 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  @ViewChild("f") signupForm: NgForm;
-  defaultQuestion = "pet";
+  @ViewChild("f", { static: false }) signupForm: NgForm;
+  defaultQuestion = "teacher";
   answer = "";
   genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = "Superuser";
@@ -35,6 +43,13 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
