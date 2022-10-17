df385b8906e4c1aab12ecb4351d06cafa06b643d
Seth Bolander <bol15015@byui.edu>
2022-10-17 11:52:03 -0600

    setValue and patchValue

--- Academind - Angular The Complete Guide/w08_FormHandling/forms-td-start/src/app/app.component.ts
+++ Academind - Angular The Complete Guide/w08_FormHandling/forms-td-start/src/app/app.component.ts
@@ -10,9 +10,24 @@ export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
   defaultQuestion = "pet";
   answer = "";
+  genders = ["male", "female"];
 
   suggestUserName() {
     const suggestedName = "Superuser";
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: "",
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   // onSubmit(form: NgForm) {
