52c345ca352d16471492560eee3d081db6b97e66
Seth Bolander <bol15015@byui.edu>
2022-10-17 15:05:45 -0600

    a bunch of reactive forms work

--- Academind - Angular The Complete Guide/w08_FormHandling/forms-reactive-start/src/app/app.component.ts
+++ Academind - Angular The Complete Guide/w08_FormHandling/forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders = ["male", "female"];
+  signupForm: FormGroup;
+  get controls() {
+    return (this.signupForm.get("hobbies") as FormArray).controls;
+  }
+  forbiddenUsernames = ["Chris", "Anna"];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl("male"),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get("hobbies")).push(control);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
+  }
 }
--- Academind - Angular The Complete Guide/w08_FormHandling/forms-reactive-start/src/app/app.module.ts
+++ Academind - Angular The Complete Guide/w08_FormHandling/forms-reactive-start/src/app/app.module.ts
@@ -1,19 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
