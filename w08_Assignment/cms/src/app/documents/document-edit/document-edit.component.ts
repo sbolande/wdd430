aab236d0915d53e78f3b1d8534f30b6e904d8583
Seth Bolander <bol15015@byui.edu>
2022-10-18 11:23:46 -0600

    updated OnInit to set EDITMODE

--- w08_Assignment/cms/src/app/documents/document-edit/document-edit.component.ts
+++ w08_Assignment/cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'app-document-edit',
@@ -12,9 +14,30 @@ export class DocumentEditComponent implements OnInit {
   document: Document;
   editMode: boolean = false;
 
-  constructor() {}
+  constructor(
+    private docService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      let id = params['id'];
+      if (id === undefined || id === null) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.docService.getDocument(id);
+      if (
+        this.originalDocument === undefined ||
+        this.originalDocument === null
+      ) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
+  }
 
   onSubmit(form: NgForm) {}
 
