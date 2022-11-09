3853bd525d5f632d1336e57d8ef5e44ca96a67f5
Seth Bolander <bol15015@byui.edu>
2022-11-09 14:11:44 -0700

    implemented contacts-filter pipe

--- w09_Assignment/cms/src/app/contacts/contact-list/contact-list.component.ts
+++ w09_Assignment/cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -17,6 +17,7 @@ import { ContactService } from '../contact.service';
 export class ContactListComponent implements OnInit, OnDestroy {
   contacts: Contact[] = [];
   subscription: Subscription;
+  term: string = '';
 
   constructor(private contactService: ContactService) {}
 
@@ -31,5 +32,9 @@ export class ContactListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  search(value: string) {
+    this.term = value;
   }
 }
