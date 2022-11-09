369a3f058eb7a1bddc16325db01d8273227066e3
Seth Bolander <bol15015@byui.edu>
2022-11-09 13:33:34 -0700

    created contacts-filter pipe

--- w09_Assignment/cms/src/app/app.module.ts
+++ w09_Assignment/cms/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { DocumentEditComponent } from './documents/document-edit/document-edit.c
 import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
 import { FormsModule } from '@angular/forms';
 import { DndModule } from 'ng2-dnd';
+import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
 
 @NgModule({
   declarations: [
@@ -39,6 +40,7 @@ import { DndModule } from 'ng2-dnd';
     DropdownDirective,
     DocumentEditComponent,
     ContactEditComponent,
+    ContactsFilterPipe,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule, DndModule.forRoot()],
   providers: [],
