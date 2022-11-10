7007d6c5469ad780a7a422a1b703dba6f48d85d9
Seth Bolander <bol15015@byui.edu>
2022-11-10 08:37:24 -0700

    finished adding firebase to message service

--- w09_Assignment/cms/src/app/contacts/contact.service.ts
+++ w09_Assignment/cms/src/app/contacts/contact.service.ts
@@ -2,7 +2,6 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { Contact } from './contact.model';
-import { MOCKCONTACTS } from './MOCKCONTACTS';
 
 @Injectable({
   providedIn: 'root',
