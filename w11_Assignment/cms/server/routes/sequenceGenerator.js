ef16a78dd52157c444002d42b534761635693ac3
Seth Bolander <bol15015@byui.edu>
2022-11-22 20:59:39 -0700

    copied over sequenceGenerator

---
+++ w11_Assignment/cms/server/routes/sequenceGenerator.js
@@ -0,0 +1,58 @@
+var Sequence = require("../models/sequence");
+
+var maxDocumentId;
+var maxMessageId;
+var maxContactId;
+var sequenceId = null;
+
+function SequenceGenerator() {
+  Sequence.findOne().exec(function (err, sequence) {
+    if (err) {
+      return res.status(500).json({
+        title: "An error occurred",
+        error: err,
+      });
+    }
+
+    sequenceId = sequence._id;
+    maxDocumentId = sequence.maxDocumentId;
+    maxMessageId = sequence.maxMessageId;
+    maxContactId = sequence.maxContactId;
+  });
+}
+
+SequenceGenerator.prototype.nextId = function (collectionType) {
+  var updateObject = {};
+  var nextId;
+
+  switch (collectionType.toLowerCase()) {
+    case "documents":
+      maxDocumentId++;
+      updateObject = { maxDocumentId: maxDocumentId };
+      nextId = maxDocumentId;
+      break;
+    case "messages":
+      maxMessageId++;
+      updateObject = { maxMessageId: maxMessageId };
+      nextId = maxMessageId;
+      break;
+    case "contacts":
+      maxContactId++;
+      updateObject = { maxContactId: maxContactId };
+      nextId = maxContactId;
+      break;
+    default:
+      return -1;
+  }
+
+  Sequence.update({ _id: sequenceId }, { $set: updateObject }, function (err) {
+    if (err) {
+      console.log("nextId error = " + err);
+      return null;
+    }
+  });
+
+  return nextId;
+};
+
+module.exports = new SequenceGenerator();
