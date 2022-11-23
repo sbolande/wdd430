1c54448aaab9a1c2da8a3fedb7a4aaf204651c95
Seth Bolander <bol15015@byui.edu>
2022-11-23 14:01:53 -0700

    added Angular Material

--- FinalProject/pokemon-quest-recipes/src/app/app.module.ts
+++ FinalProject/pokemon-quest-recipes/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeCreateComponent } from './recipes/recipe-create/recipe-create.component';
 import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
