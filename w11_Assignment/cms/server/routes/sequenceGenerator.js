36460ad91d5b62d42f372f05fadcd6387bbd9920
Seth Bolander <bol15015@byui.edu>
2022-11-22 22:23:21 -0700

    removed deprecated update method

--- w11_Assignment/cms/server/routes/sequenceGenerator.js
+++ w11_Assignment/cms/server/routes/sequenceGenerator.js
@@ -45,12 +45,12 @@ SequenceGenerator.prototype.nextId = function (collectionType) {
       return -1;
   }
 
-  Sequence.update({ _id: sequenceId }, { $set: updateObject }, function (err) {
-    if (err) {
+  Sequence.updateOne({ _id: sequenceId }, { $set: updateObject }).catch(
+    (err) => {
       console.log("nextId error = " + err);
       return null;
     }
-  });
+  );
 
   return nextId;
 };
